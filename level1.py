d4085d9542cb8510984173a6c50b68806f989b42
player89087 <edar.horst@gmail.com>
2025-05-02 21:02:31 +0200

    fixed level 1 using modulo

--- level1.py
+++ level1.py
@@ -1,23 +1,21 @@
-letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
-                   'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+           'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+message_inp = input("The secret message: ").lower()
+codec = int(input("Type in the codec, by how many letters was the word shifted?: "))
 
-message = []
-message_inp = input("The secret message: ")
-message.append(message_inp)
-codec = input("Type in the codec, by how many letter was the word shifted ?: ")
+encrypted_message = []
 
+for char in message_inp:
+    if char == ' ':
+        encrypted_message.append(' ')
+    elif char in letters:
+        original_pos = letters.index(char)
+        new_pos = (original_pos + codec) % 26  # Wrap around using modulo 26
+        encrypted_message.append(letters[new_pos])
+    else:
+        encrypted_message.append(char)  # Keep non-alphabet characters as-is
 
-y = 0
-x = len(message)
-for i in range (0,x):
-    shift = message[y]
-    print(shift)
-    y += 1
-    #shift = message.index(y)
-    #if shift  in letter:
-    z = letter.index(shift) # should find the index from the letter from the message in the letter list
-    print(z)
-    codec = int(codec)
-    z += codec
-    print(letter[z])
+print("Encrypted message:", ''.join(encrypted_message))
+
+"http://www.pythonchallenge.com/pc/def/map.html".maketrans()
