74e172ec3642237c09be27296e40b2d92b40f886
player89087 <edar.horst@gmail.com>
2025-05-21 19:25:05 +0200

    started level 8, data is compressed with bz2

--- level1.py
+++ level1.py
@@ -1,3 +1,6 @@
+import string 
+
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
@@ -11,11 +14,19 @@ for char in message_inp:
         encrypted_message.append(' ')
     elif char in letters:
         original_pos = letters.index(char)
-        new_pos = (original_pos + codec) % 26  # Wrap around using modulo 26
+        new_pos = (original_pos + codec) % 26  
         encrypted_message.append(letters[new_pos])
     else:
-        encrypted_message.append(char)  # Keep non-alphabet characters as-is
-
+        encrypted_message.append(char) 
 print("Encrypted message:", ''.join(encrypted_message))
 
-"http://www.pythonchallenge.com/pc/def/map.html".maketrans()
+table = string.maketrans(
+    "abcdefghijklmnopqrstuvwxyz", "cdefghijklmnopqrstuvwxyzab"
+)
+
+int = "abcdefghiklmnopqrstuvwxyz"
+out = "cdefghiklmnopqrstuvwxyzab"
+
+trans = string.maketrans(int, out)
+b = map # the url 
+print(b.translate(trans))
