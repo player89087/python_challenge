284da2501f02e9532334d59ad8e7e659021479b5
player89087 <edar.horst@gmail.com>
2025-05-21 19:48:04 +0200

    fixed the error, string had to b string for binary, completed level 8

--- level8.py
+++ level8.py
@@ -1,8 +1,11 @@
 import bz2 
-
+# we got a picture of a fly that make bzzzz --> bz2 compressed files 
 # BZh for bzip2 
-user =  'BZh91AY&SYA\xaf\x82\r\x00\x00\x01\x01\x80\x02\xc0\x02\x00 \x00!\x9ah3M\x07<]\xc9\x14\xe1BA\x06\xbe\x084'
-password = 'BZh91AY&SY\x94$|\x0e\x00\x00\x00\x81\x00\x03$ \x00!\x9ah3M\x13<]\xc9\x14\xe1BBP\x91\xf08'
 
+# if click on picture input for credentials 
+user =  b'BZh91AY&SYA\xaf\x82\r\x00\x00\x01\x01\x80\x02\xc0\x02\x00 \x00!\x9ah3M\x07<]\xc9\x14\xe1BA\x06\xbe\x084' # b for binary otherwise error
+password = b'BZh91AY&SY\x94$|\x0e\x00\x00\x00\x81\x00\x03$ \x00!\x9ah3M\x13<]\xc9\x14\xe1BBP\x91\xf08'
 
-bz2.decompress(user)
+# decompress them 
+print(bz2.decompress(user).decode('utf-8'))
+print(bz2.decompress(password).decode('utf-8'))
